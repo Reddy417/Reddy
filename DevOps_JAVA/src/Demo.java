bba164a2db5443c672a5223729fdbd3bac3e7ec3
Reddy417 <43058566+Reddy417@users.noreply.github.com>
2018-09-09 16:16:31 +0530

    Update Demo.java

--- DevOps_JAVA/src/Demo.java
+++ DevOps_JAVA/src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Heloo Reddy , how are you , yess");
+		System.out.println("Heloo Reddy , i ahve pushed some data");
 	}
 
 }
