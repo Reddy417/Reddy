f553333ad42be8801a7602793af1ca34710c8453
Akhila <akhila.n@slkgroup.com>
2018-09-09 15:26:41 +0530

    i am pushing it

--- DevOps_JAVA/src/Demo.java
+++ DevOps_JAVA/src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Heloo Reddy");
+		System.out.println("Heloo Reddy , how are you , yess");
 	}
 
 }
